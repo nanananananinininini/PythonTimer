deb792dd40872c1f8f0f1a402bd7d97dba53745c
bigmonkey_is_a_juicer <1494366612@qq.com>
2026-02-11 14:52:16 +0800

    Version 1.0.2
    I. 删除暂时用不到的os库
    II. 让读数更精确
    III. 结尾保留界面15s

--- PythonTimer.py
+++ PythonTimer.py
@@ -1,5 +1,5 @@
-import os
 import time
+import datetime
 
 def timeConvert(duration_seconds):
     total = int(duration_seconds)
@@ -9,17 +9,18 @@ def timeConvert(duration_seconds):
     return f"{hours:02}:{minutes:02}:{seconds:02}"
 
 
-start_time = time.time()
-print("Timer Started.")
+start_time = time.perf_counter()
+start_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+print(f"Timer Started.")
 try:
     while True:
         time.sleep(1)
-        NowTimeCode = time.time()
-        print(f"\rElasped:{timeConvert(NowTimeCode - start_time)}", end="", flush=True)
+        NowTimeCode = time.perf_counter()
+        print(f"\rElapsed: {timeConvert(NowTimeCode - start_time)}", end="", flush=True)
 
 except KeyboardInterrupt:
     print("")
-    ending_time = time.time()
+    ending_time = time.perf_counter()
     duration = ending_time - start_time
 
     print("Timer Stopped", end=", duration = ")
@@ -27,3 +28,6 @@ except KeyboardInterrupt:
 
     with open("DurationList.txt", 'a', encoding='utf-8') as f:
         f.write(timeConvert(duration) + "\n")
+
+    print("Will exit in 15 seconds.")
+    time.sleep(15)

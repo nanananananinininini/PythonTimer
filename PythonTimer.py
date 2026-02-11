33d30d30cbc9d2f4b97ff2d7ca96e4fece2e7fef
bigmonkey_is_a_juicer <1494366612@qq.com>
2026-02-11 12:19:24 +0800

    最初版：包含正计时 + 实时时间显示

--- PythonTimer.py
+++ PythonTimer.py
@@ -1,1 +1,27 @@
+import os
+import time
 
+def timeConvert(duration_seconds):
+    total = int(duration_seconds)
+    seconds = total % 60
+    minutes = (total // 60) % 60
+    hours = total // 3600
+    return f"{hours:02}:{minutes:02}:{seconds:02}"
+
+
+start_time = time.time()
+print("Timer Started.")
+try:
+    while True:
+        time.sleep(1)
+        print(timeConvert(time.time() - start_time))
+
+except KeyboardInterrupt:
+    ending_time = time.time()
+    duration = ending_time - start_time
+
+    print("Timer Stopped.", end=", duration = ")
+    print(timeConvert(duration)
+
+    with open("DurationList.txt", 'a', encoding='utf-8') as f:
+        f.write(timeConvert(duration) + "\n")

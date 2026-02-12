01ce5b0af7a622f3b541e144a021558b221e8e17
bigmonkey_is_a_juicer <1494366612@qq.com>
2026-02-12 20:23:00 +0800

    V1.1.3发行版
    1. 更改输出样式，让输出更美观
    2. 增加暂停功能（尚无写入文档），p暂停r开始
    3. 让按键反应更加迅速
    
    已发行exe

--- PythonTimer.py
+++ PythonTimer.py
@@ -1,4 +1,3 @@
-from io import StringIO
 import time
 import datetime
 import keyboard
@@ -13,28 +12,42 @@ def timeConvert(duration_seconds):
 def lapWriter(duration_seconds):
     listing_time.append(duration_seconds)
     with open("DurationList.txt", 'a', encoding='utf-8') as f:
+
         f.write("L")
         f.write(str((len(listing_time))))
         f.write('_')
         f.write(listing_time[-1] + '\n')
+
 start_time = time.perf_counter()
 start_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
 print(f"Timer Started.")
 
 try:
     listing_time = []
 
     while True:
-        time.sleep(1)
+        time.sleep(0.00000000000001) 
         NowTimeCode = time.perf_counter()
-        print(f"\rElapsed: {timeConvert(NowTimeCode - start_time)}", end="", flush=True)
-        if(keyboard.is_pressed('l')):
+        print(f"\rElapsed: {timeConvert(NowTimeCode - start_time)} ", end="", flush=True)
+        
+        if keyboard.is_pressed('l'):
             lapWriter(timeConvert(NowTimeCode - start_time))
-            print(f"laps:{len(listing_time)}", flush=True) # Changed from listing_time.count() to len(listing_time)
+            print(f",laps:{len(listing_time)}", flush=True) 
+            while keyboard.is_pressed('l'): pass
 
-       
-except ImportError:
-    print("You may not use the Lap function or more features.")
+        if keyboard.is_pressed("p"):
+            pausestart = time.perf_counter()
+            
+            while not keyboard.is_pressed('r'):
+                print(f"\rElapsed: {timeConvert(pausestart - start_time)} ,Paused", end="", flush=True)
+                time.sleep(0.01)
+            
+            pauseend = time.perf_counter()
+
+            start_time += (pauseend - pausestart)
+            print("\r" + " " * 50 + "\r", end="", flush=True)
+            while keyboard.is_pressed('r'): pass 
 
 except KeyboardInterrupt:
     print("")
